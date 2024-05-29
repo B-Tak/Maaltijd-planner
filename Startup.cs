66b37651dd10e40ba033fc05fbe057877998dd24
Btak <Bastiaan.Tak@proton.me>
2024-05-29 16:00:32 +0200

    Changed pages into razor pages and made pages directory

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
-
+app.MapRazorPages();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 app.Run();
