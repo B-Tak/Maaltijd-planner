5f9a74fa6fe22ca70fdd9b46da452d2df9f49419
Bastiaan <128037118+B-Tak@users.noreply.github.com>
2024-05-30 23:23:33 +0200

    Translated the current day into dutch and added comments to the code

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,8 +5,11 @@ public class IndexModel
 {
     public static string GetDay()
     {
+        // I: Obtains the current day, also adds the dutch language.
         DayOfWeek today = DateTime.Today.DayOfWeek;
-        string t = today.ToString();
-        return t;
+        CultureInfo dutch = new CultureInfo("nl-NL");
+        // I: Translates the English day name into Dutch.
+        string dutchDayName = dutch.DateTimeFormat.GetDayName(today);
+        return dutchDayName;
     }
 }
