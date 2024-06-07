d3c09eec6e0e27257471703876121cde328e71dc
TDoggen <177013@student.horizoncollege.nl>
2024-06-07 13:33:49 +0200

    Recipe/RecipeAdd pages added

---
+++ Pages/Recepten.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MaaltijdPlanner
+{
+    public class ReceptenModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
